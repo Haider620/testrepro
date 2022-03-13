bd1b86b6d916f0d56a60562167434e610e8b40fe
Haider620 <101508191+Haider620@users.noreply.github.com>
2022-03-13 19:33:02 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+def percent (marks):
+    p = (sum(marks)/500)*100
+    return p
+
+
+
+
+marks1 = [45,90 ,90,56 ,79]
+percentage = percent(marks1)
+print(percentage)
+
+marks2 =[45,56,78,90,76]
+percentage = percent(marks2)
+print(percentage)
+
+
+marks3 = [67 ,67, 78,67,67]
+percentage = percent(marks3)
+print(percentage)
